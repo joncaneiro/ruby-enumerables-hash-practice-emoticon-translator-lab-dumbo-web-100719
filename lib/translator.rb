b05caea11b9f58d3478ec2c28120d4f736b3f1a3
Jon Caneiro <joncaneiro@gmail.com>
2019-09-30 17:16:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,7 +28,7 @@ def get_japanese_emoticon(file_path, emoticon)
   
   emoticons = load_library(file_path)
   
-  emoticons.each do |key1, key2|
+  emoticons.each do |key, value|
    
    binding.pry
   
